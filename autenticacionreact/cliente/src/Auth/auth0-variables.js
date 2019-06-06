3b2b336328e01edbc3d79a49ba9878f2d69b496e
Antonio <antonio_dm93@hotmail.com>
2019-06-06 21:42:18 +0200

    creo autenticacion

---
+++ autenticacionreact/cliente/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'antonio-delgado.eu.auth0.com',
+  clientId: '2dobImxeoLdDlRRdqPS33Ys7g4ACylRa',
+  callbackUrl: 'http://localhost:3000/callback', 
+  apiURL: 'http://productos'
+}
